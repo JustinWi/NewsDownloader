3fbc62bddcd153840f426dfbea454573e5bece50
Justin Wilcox <justin@needtobounce.com>
2013-09-26 12:12:26 -0700

    Turning on Google Analytics

--- js/index.js
+++ js/index.js
@@ -293,13 +293,13 @@ $(document).ready(function() {
 		urlToShare: 'http://wp.me/p2pmCq-gA'
 	});
 		
-//	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-//	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-//	
-//	  ga('create', 'UA-44161122-1', 'customerdevlabs.com');
-//	  ga('send', 'pageview');
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-44161122-1', 'customerdevlabs.com');
+	  ga('send', 'pageview');
 	
 	$('#news-search').keypress(function (e) {
 		  if (e.which == 13) {
